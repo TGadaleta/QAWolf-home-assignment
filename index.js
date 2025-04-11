11bb125c0fe043866f7c2492c88b55494a9860ad
Tony Gadaleta <anthonygadaleta@gmail.com>
2025-04-11 14:32:29 -0400

    Refactor article collection logic in sortHackerNewsArticles function

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 // EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
 import { chromium } from "playwright";
-import { test, expect } from "@playwright/test";
 
-
-async function sortHackerNewsArticles(){
-  // launch browser
+async function sortHackerNewsArticles() {
   const browser = await chromium.launch({ headless: false });
   const context = await browser.newContext();
   const page = await context.newPage();
+
   const articles = [];
-  
-  // navigate to Hacker News
+
+  // Navigate to Hacker News "newest"
   await page.goto("https://news.ycombinator.com/newest");
 
-  // collect the articles on the first page
-  const articleRows = await page.locator(".athing").all();
+  while (articles.length < 100) {
+    const articleRows = await page.locator(".athing").all();
+
     for (const row of articleRows) {
-      while (articles.length < 100) {
-        const title = await row.locator(".titleline").innerText();
-        const subtextRow = await row.locator('xpath=following-sibling::tr[1]');
-        const timeString = await subtextRow.locator(".age").getAttribute("title");
-        const time = new Date(timeString.split(" ")[0]);
-        articles.push({ title, time });
-      }
-    // click on the "More" button
+      if (articles.length >= 100) break;
+
+      const id = await row.getAttribute("id");
+
+      const subtextRow = await row.locator("xpath=following-sibling::tr[1]");
+      const timeString = await subtextRow.locator(".age").getAttribute("title");
+
+      const time = new Date(timeString.split(" ")[0]);
+      articles.push({ id, time });
+
+      console.log(id, time.toISOString());
+    }
+
     if (articles.length < 100) {
-      const moreButton = await page.getByRole("link", { name: "More"}).click()
+      await page.getByRole("link", { name: "More" }).click();
     }
   }
 
-  // sort the articles by time in new array
-  const sortedArticles = articles.sort((a, b) => b.time - a.time);
-  
-  console.log("Number of Articles: ", sortedArticles.length);
-
+  console.log("Collected", articles.length, "articles.");
   await browser.close();
-};
+}
 
 (async () => {
   await sortHackerNewsArticles();

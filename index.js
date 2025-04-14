f18bb570d3bdac193d8cab83409f8078091511e4
Tony Gadaleta <anthonygadaleta@gmail.com>
2025-04-14 13:57:28 -0400

    Refactor article collection logic into separate collectArticles function

--- index.js
+++ index.js
@@ -1,37 +1,15 @@
 // EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
 import { chromium } from "playwright";
+import collectArticles from "./tests/wolf_qa_script.js";
 
 async function sortHackerNewsArticles() {
   const browser = await chromium.launch({ headless: false });
   const context = await browser.newContext();
   const page = await context.newPage();
 
-  const articles = [];
+  //get the articles using the collectArticles function
+  const articles = await collectArticles(page, []);
 
-  // Navigate to Hacker News "newest"
-  await page.goto("https://news.ycombinator.com/newest");
-
-  while (articles.length < 100) {
-    const articleRows = await page.locator(".athing").all();
-
-    for (const row of articleRows) {
-      if (articles.length >= 100) break;
-
-      const id = await row.getAttribute("id");
-
-      const subtextRow = await row.locator("xpath=following-sibling::tr[1]");
-      const timeString = await subtextRow.locator(".age").getAttribute("title");
-
-      const time = new Date(timeString.split(" ")[0]);
-      console.log(time)
-      articles.push({ id, time });
-    }
-
-
-    if (articles.length < 100) {
-      await page.locator(".morelink").click();
-    }
-  }
   // Close the browser
   await browser.close();
 
